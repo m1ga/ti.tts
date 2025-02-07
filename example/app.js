f3bef4d0b1253ed9eebff83a57e459a99955e934
Informate.it <40699080+Informate@users.noreply.github.com>
2025-02-07 13:34:05 +0100

    Update app.js

--- example/app.js
+++ example/app.js
@@ -1,4 +1,5 @@
 const tts = require("ti.tts");
+
 const win = Ti.UI.createWindow();
 
 win.open();
@@ -6,10 +7,25 @@ win.open();
 win.addEventListener("open", function() {
 	tts.init();
 });
+win.addEventListener("close", function() {
+	tts.shutdown();
+});
 tts.addEventListener("init", function(e) {
-	tts.speak("Hello");
-	tts.synthesizeToFile("Hello");
+        tts.emitEvents();
+	tts.speak({text:"Hello! This is a test message for you."});
+//	tts.synthesizeToFile("Hello");
+ console.log(e);
+ console.log(tts.getVoices().split("|"));
+});
+tts.addEventListener("start", function(e) {
+	console.log(e.type,e.id);
+});
+tts.addEventListener("stop", function(e) {
+	console.log(e.type,e.id,interrupted);
+});
+tts.addEventListener("error", function(e) {
+	console.log(e.type,e.id,code);
 });
 tts.addEventListener("done", function(e) {
-	console.log(e.blob);
+	console.log(e.type,e.id);
 });
